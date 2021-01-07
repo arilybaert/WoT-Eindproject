adadabacbbb0711a9eb6c28fc026c8e843dd3dfb
Ari Lybaert <arilybae@student.arteveldehs.be>
2021-01-07 23:30:19 +0100

    did some code refactoring

--- app/classes/db.py
+++ app/classes/db.py
@@ -1,5 +1,7 @@
 import mysql.connector
 import os
+import requests
+import json
 
 from dotenv import load_dotenv
 from datetime import datetime, date
@@ -55,6 +57,30 @@ class DB(object):
         self.cursor.execute(query, values)
         self.db.commit()
 
+
+    def get_student_info(self, id):
+        print("this is a new student, creating new student row ...")
+        # db.write_data(put_new_student_query, put_new_student_value)
+
+        # RETRIEVE JWT token
+        post_token = os.getenv('API_URL_TOKEN')
+        body = {'grant_type': os.getenv('GRANT_TYPE'), 'client_id': os.getenv('CLIENT_ID'), 'resource': os.getenv('RESOURCE'), 'client_secret': os.getenv('CLIENT_SECRET')}
+        token_res = requests.post(post_token, data = body) # save request results
+        token_res_obj = json.loads(token_res.text) # convert results to python object
+
+        # convert studentID to readable ID
+        altered_student_id = "".join(reversed([id[i:i+2] for i in range(0, len(id), 2)]))
+        decimal_id = int(altered_student_id, 16)
+
+        # RETRIEVE student information
+        post_student = os.getenv('API_URL_STUDENT_PREFIX') + str(decimal_id) + os.getenv('API_URL_STUDENT_SUFFIX')
+        headers = {
+            'Authorization': 'Bearer ' + token_res_obj['access_token']
+        }
+        student_info_res = requests.get(post_student, headers = headers) # save request results
+        student_res_obj = json.loads(student_info_res.text) # convert results to python object
+        
+        return student_res_obj
 
     def close_connection(self):
         self.db.close()
--- app/main.py
+++ app/main.py
@@ -6,20 +6,17 @@ from classes.db import DB
 from classes.pn7150 import PN7150
 from datetime import datetime, date
 import json
-import requests
-import os
 
 
 now = datetime.now()
 today = date.today()
 db = DB()
+pn7150 = PN7150()
 
 # set current date and time
 current_time = now.strftime("%H:%M:%S")
 current_date = today.strftime("%Y-%m-%d")
 
-
-pn7150 = PN7150()
 
 filename = "/home/pi/Desktop/config.txt"
 
@@ -74,12 +71,13 @@ if datastore["exam_mode"] == "1":
     print("exam mode is on")
     exam = 1
 
-
+#
+# Read nfc cards and write every scan to the db. IF student doesn't exists, it retrieves the info with the help of the Artevelde API
+#
 
 def text_callback(text):
     db = DB()
     student_id = text.replace(" ", "")
-
 
     # QUERY to check if student exists
     find_student_query = ("SELECT COUNT(nfc_id) FROM students WHERE nfc_id = %s")
@@ -87,38 +85,18 @@ def text_callback(text):
 
 
     if db.get_data(find_student_query, find_student_value) == 0:
-        print("this is a new student, creating new student row ...")
-        # db.write_data(put_new_student_query, put_new_student_value)
 
-        # RETRIEVE JWT token
-        post_token = os.getenv('API_URL_TOKEN')
-        body = {'grant_type': os.getenv('GRANT_TYPE'), 'client_id': os.getenv('CLIENT_ID'), 'resource': os.getenv('RESOURCE'), 'client_secret': os.getenv('CLIENT_SECRET')}
-        token_res = requests.post(post_token, data = body) # save request results
-        token_res_obj = json.loads(token_res.text) # convert results to python object
-
-        # convert studentID to readable ID
-        altered_student_id = "".join(reversed([student_id[i:i+2] for i in range(0, len(student_id), 2)]))
-        decimal_id = int(altered_student_id, 16)
-
-        # RETRIEVE student information
-        post_student = os.getenv('API_URL_STUDENT_PREFIX') + str(decimal_id) + os.getenv('API_URL_STUDENT_SUFFIX')
-        headers = {
-            'Authorization': 'Bearer ' + token_res_obj['access_token']
-        }
-        student_info_res = requests.get(post_student, headers = headers) # save request results
-        student_res_obj = json.loads(student_info_res.text) # convert results to python object
-
+        # RETRIEVE Student info via the Artevelde API
+        student_res_obj = db.get_student_info(student_id)
 
         #   QUERY to create new student
         put_new_student_query = ("INSERT INTO students (firstname, lastname, email, nfc_id) VALUES (%s,%s,%s,%s)")
         put_new_student_value = (student_res_obj['Voornaam'], student_res_obj['Naam'], student_res_obj['Email'], student_id)
 
+
         db.write_data(put_new_student_query, put_new_student_value) # write new student to db
 
     
-
-
-
     # query FIND CLASSROOM
     find_class_query = ("SELECT id FROM classrooms WHERE title = %s")
     find_class_value =((str(classroom.decode('unicode_escape').encode('ascii', 'utf-8')), ))
@@ -127,7 +105,6 @@ def text_callback(text):
     put_students_query = ("INSERT INTO classrooms__students (scan_date, scan_time, classroom_id, student_id, exam) VALUES (%s,%s,%s,%s,%s)")
     put_students_value = (current_date, current_time, db.get_data(find_class_query, find_class_value), student_id, exam)
     
-
     print(student_id)
     
     db.write_data(put_students_query, put_students_value)
@@ -135,8 +112,6 @@ def text_callback(text):
 
 pn7150.when_tag_read = text_callback
 pn7150.start_reading()
-
-
 
 
 db.close_connection()
